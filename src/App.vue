3f6289d06ef6b4c01a56f987aac77e3976c0bf22
Favour Emeka <favorodera@gmail.com>
2026-01-31 11:48:42 +0100

    chore(config): Initialize project with default configuration files

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { Button } from './components/button';
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+
+      <Button>Button</Button>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
